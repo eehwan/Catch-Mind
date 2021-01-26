5dc90fdfa1a4b2b2728c2c92e113866facbcff07
eehwan <eehwan.shin@gmail.com>
2021-01-26 23:58:28 +0900

    made functions (setNickname, sendMessage)

--- src/server.js
+++ src/server.js
@@ -1,14 +1,29 @@
 import express from "express";
 import path from "path";
 import socketIO from "socket.io";
+import morgan from "morgan";
 
 const PORT = 5006
 const app = express();
 
 const handleListening = () => console.log(`✔ Listening on PORT: ${PORT}`);
 
-app.listen(PORT, handleListening);
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use("/static", express.static(path.resolve(__dirname, "static")));
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => res.render("home"));
+
+const server = app.listen(PORT, handleListening);
+const io = socketIO(server);
+
+io.on("connection", socket => {
+    socket.on("newMessage", ({ message }) => {
+        socket.broadcast.emit("messageAnnounce", { message, nickname: socket.nickname || socket.id });
+    });
+    socket.on("setNickname", ({ nickname }) => {
+      socket.nickname = nickname;
+      socket.broadcast.emit("messageAnnounce", { message: `${socket.id} changed Nickname to "${socket.nickname}"`, nickname: "System" });
+    });
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,18 @@
-index;
+const socket = io("/");
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`You: ${message}`);
+}
+
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+  console.log(`Nickname changed to "${nickname}"`);
+}
+
+function handleMessageAnnounce(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageAnnounce", handleMessageAnnounce);
