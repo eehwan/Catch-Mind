777a4a03d12577e683d1bb4f818c16e94399c874
eehwan <eehwan.shin@gmail.com>
2021-01-28 16:06:36 +0900

    made more functions & fixed small bugs

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const handleListening = () => console.log(`✔ Listening on PORT: ${PORT}`);
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
-app.use("/static", express.static(path.resolve(__dirname, "static")));
+app.use(express.static(path.resolve(__dirname, "static")));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => res.render("home"));
@@ -19,11 +19,12 @@ const server = app.listen(PORT, handleListening);
 const io = socketIO(server);
 
 io.on("connection", socket => {
-    socket.on("newMessage", ({ message }) => {
-        socket.broadcast.emit("messageAnnounce", { message, nickname: socket.nickname || socket.id });
-    });
-    socket.on("setNickname", ({ nickname }) => {
-      socket.nickname = nickname;
-      socket.broadcast.emit("messageAnnounce", { message: `${socket.id} changed Nickname to "${socket.nickname}"`, nickname: "System" });
-    });
+  socket.on("enter", () => socket.broadcast.emit("systemAnnounce", { message: `${socket.id} entered`}))
+  socket.on("newMessage", ({ message }) => {
+      socket.broadcast.emit("messageAnnounce", { message, nickname: socket.nickname || socket.id });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+    socket.broadcast.emit("systemAnnounce", { message: `${socket.id} changed Nickname to "${socket.nickname}"`});
+  });
 });
--- src/static/index.js
+++ src/static/index.js
@@ -4,15 +4,18 @@ function sendMessage(message) {
   socket.emit("newMessage", { message });
   console.log(`You: ${message}`);
 }
-
 function setNickname(nickname) {
   socket.emit("setNickname", { nickname });
   console.log(`Nickname changed to "${nickname}"`);
 }
-
+function handleSystemAnnounce({ message }) {
+  console.log(`<<  ${message}  >>`);
+}
 function handleMessageAnnounce(data) {
   const { message, nickname } = data;
   console.log(`${nickname}: ${message}`);
 }
 
+socket.emit("enter")
+socket.on("systemAnnounce", handleSystemAnnounce);
 socket.on("messageAnnounce", handleMessageAnnounce);
