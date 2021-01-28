265d198cd7e71438ee506a2a42501ddfe5f401bc
eehwan <eehwan.shin@gmail.com>
2021-01-28 21:48:10 +0900

    added gulp-csso, gulp-browserify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,9 @@
 import gulp from "gulp";
 import sass from "gulp-sass";
 import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
+import del from "del";
+import bro from "gulp-browserify";
 
 const paths = {
   styles: {
@@ -8,10 +11,16 @@ const paths = {
     dest: "src/static",
     watch: "assets/scss/**/*.scss"
   },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static",
+    watch:"assets/js/**/*.js"
+  }
 };
 
-function styles() {
-  return gulp
+const clean = () => del(["src/static"])
+const styles = () =>
+  gulp
     .src(paths.styles.src)
     .pipe(sass())
     .pipe(
@@ -20,12 +29,20 @@ function styles() {
         castcade: false
       })
     )
+    .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
-}
-function watchFiles() {
-  gulp.watch(paths.styles.watch, styles)
+
+const js = () =>
+      gulp
+        .src(paths.js.src)
+        .pipe(bro())
+        .pipe(gulp.dest(paths.js.dest))
+
+const watchFiles = () => {
+  gulp.watch(paths.styles.watch, styles);
+  gulp.watch(paths.js.watch);
 }
 
-const dev = gulp.series([styles, watchFiles])
+const dev = gulp.series(clean, styles, js, watchFiles)
 
 export default dev;
