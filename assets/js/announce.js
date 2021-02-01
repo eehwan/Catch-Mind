be671f916c693143d841e5808bdf3b948bd62bf1
eehwan <eehwan.shin@gmail.com>
2021-02-01 23:27:34 +0900

    visualized announce

--- assets/js/announce.js
+++ assets/js/announce.js
@@ -1,11 +1,22 @@
+const announces = document.querySelector("#jsAnnounce");
+const systemAnnounces = document.querySelector(".systemAnnounces");
 export function handleSystemAnnounce({ message }) {
+    const systemAnnounce = document.createElement("div");
+    systemAnnounce.innerText = message;
+    systemAnnounces.appendChild(systemAnnounce);
     console.log(`<< System >>\n${message}`);
 }
 export function handleMessageAnnounce(data) {
     const { message, nickname } = data;
-    console.log(`${nickname}: ${message}`);
+    const announce = document.createElement("div");
+    announce.innerText = `${nickname}: ${message}`;
+    announce.style.color = "#000000";
+    announces.appendChild(announce);
 }
 export function sendMessage(message) {
     window.socket.emit(window.events.newMessage, { message });
-    console.log(`You: ${message}`);
+    const announce = document.createElement("div");
+    announce.innerText = `You: ${message}`;
+    announce.style.color = "#353535";
+    announces.appendChild(announce);
 }
