3816d85950e3e27ec884324621dec428c0916c1c
eehwan <eehwan.shin@gmail.com>
2021-02-07 16:16:26 +0900

    styling & edited chat.js

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,12 +1,17 @@
 const chatMessages = document.querySelector(".chatMessages");
-const chatForm = document.querySelector("chatForm");
+const chatForm = document.querySelector("#chatForm");
+
+const appendMessage = (text, nickname) => {
+    const li = document.createElement(li);
+    li.className(nickname? "other" : "self");
+    li.innerHTML = `${nickname ? nickname : "You"} : ${text}`;
+    chatMessages.appendChild(li);
+}
 
 const handleChatSubmit = (e) => {
     e.preventDefault();
-    const chatInput = document.querySelector("chatInput");
-    const li = document.createElement(li);
-    li.innerText = `You: ${chatInput.value}`;
-    chatForm.appendChild(li);
+    const chatInput = document.querySelector("#chatInput");
+    appendMessage(chatInput.value);
     chatInput.value = ""
 }
 
